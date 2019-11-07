9f4f66920ae3c83d4b20e0f85af736760b64a1db
winkleman <jwinks18@gmail.com>
2019-11-06 21:59:09 -0600

    new

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -13,6 +13,16 @@ const comboSum = (arr, sum) => {
   let total = arr.reduce((a, b) => a + b);
   return total === sum;
 }
+
+// const comboSum1 = (arr, sum) => {
+//   let total = arr.reduce((a, b) => a + b);
+//   return total === sum
+//   }
+  
+//   let test = [1,2,3,8,21]
+  
+//   console.log(comboSum1(test,10))
+
 module.exports = {
   isEven,
   sum,
